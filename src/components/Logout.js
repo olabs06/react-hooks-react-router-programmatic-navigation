5e9a7c2b19bd232ba017577c0494e19beaa220d6
Simeon <olabs06@yahoo.com>
2022-08-09 23:19:59 +0100

    reviewing programmatic navigation

---
+++ src/components/Logout.js
@@ -0,0 +1,22 @@
+// import { useHistory } from "react-router-dom";
+
+// function NavBar({ onLogout }) {
+//   const history = useHistory();
+
+//   function handleClick() {
+//     // logout the user
+//     onLogout();
+//     // then navigate them to the login page
+//     history.push("/login");
+//   }
+
+//   return (
+//     <nav>
+//       <button onClick={handleClick}>Logout</button>
+//     </nav>
+//   );
+// }
+
+
+
+// export default NavBar;
